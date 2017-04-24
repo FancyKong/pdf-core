88a2a55c8ab9fac89029769b383150b0913e403d
Cherish <785427346@qq.com>
2017-04-24 17:37:05 +0800

    著作者申请页面

--- src/main/java/com/cafa/pdf/core/dal/entity/Author.java
+++ src/main/java/com/cafa/pdf/core/dal/entity/Author.java
@@ -18,20 +18,50 @@ import static javax.persistence.GenerationType.IDENTITY;
 public class Author implements java.io.Serializable {
 
     private static final long serialVersionUID = -2352745039383411872L;
+    /*姓名、性别、出生年、电话、籍贯、单位、学历、职称、职务、邮箱*/
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "id", unique = true, nullable = false)
     private Long id;
-
-    @JsonIgnore
-    @Column(name = "password", nullable = false, length = 40)
-    private String password;
-
-    @Column(name = "nickname", nullable = false, length = 16)
+    /**
+     * 姓名
+     */
     private String nickname;
-
-    @Column(name = "telephone", nullable = false, length = 11)
+    /**
+     * 手机
+     */
     private String telephone;
+    /**
+     * 邮箱
+     */
+    private String email;
+    /**
+     * 密码
+     */
+    @JsonIgnore
+    private String password;
+    /**
+     * 性别
+     */
+    private String gender;
+    /**
+     * -1: 申请状态
+     * 0：冻结
+     * 1：激活
+     */
+    @Column(name = "is_active", nullable = false)
+    private Integer active;
+    /**
+     * 出生年、籍贯、学历、单位、职称、职务
+     */
+    @Column(name = "birth_year")
+    private String birthYear;
+    @Column(name = "birth_place")
+    private String birthPlace;
+    private String education;
+    private String company;
+    private String job;
+    private String duties;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_time", nullable = false, length = 19)
@@ -41,7 +71,5 @@ public class Author implements java.io.Serializable {
     @Column(name = "modified_time", nullable = false, length = 19)
     private Date modifiedTime;
 
-    @Column(name = "is_active", nullable = false)
-    private Integer active;
 
 }
